e879d1a568ac38cdf6fa538672deed6b80ef13a4
shoeb-saquib <shoeb.saquib@icloud.com>
2025-10-23 20:39:17 +0000

    Added stabilizer logic to gz_standing

--- src/prairie_control/helpers/stabilizer.py
+++ src/prairie_control/helpers/stabilizer.py
@@ -1,10 +1,17 @@
 import mujoco as mj
 import numpy as np
+from mujoco import viewer
 
 
 def body_pos(model, data, body_name):
     bid = mj.mj_name2id(model, mj.mjtObj.mjOBJ_BODY, body_name)
     return data.xpos[bid]
+
+def calculate_hip_yaw_pos(accel):
+    accel /= np.linalg.norm(accel)
+    yaw = np.arctan2(accel[1], accel[2])
+    yaw = np.clip(yaw, -1, 1)
+    return yaw
 
 def get_max_clip_in_floor(frame, corners, robot_center, threshold):
     error = None
@@ -19,42 +26,34 @@ def get_max_clip_in_floor(frame, corners, robot_center, threshold):
     return error
 
 
-class SimpleStabilizer:
+class Stabilizer:
 
-    def __init__(self, model, data):
-        self.model = model
-        self.data = data
+    def __init__(self):
+        self.model = mj.MjModel.from_xml_path("models/nemo/nemo5_nostl.xml")
+        self.data = mj.MjData(self.model)
+        self.viewer = viewer.launch_passive(self.model, self.data)
         self.kp = 3
 
-        self.m = np.sum(model.body_mass)
+        self.m = np.sum(self.model.body_mass)
         self.g = 9.81
-        self.com = data.subtree_com[0]
+        self.com = self.data.subtree_com[0]
 
         self.pitch_torque = 0
         self.roll_torque = 0
         self.hip_yaw_pos = 0
 
-    def calculate_pitch_torque(self, pz_req, robot_center):
-        torque = 0.5 * self.m * self.g * (pz_req - robot_center[0])
+    def calculate_pitch_torque(self, pz_req):
+        torque = 0.5 * self.m * self.g * pz_req
         return self.kp * torque
 
-    def calculate_roll_torque(self, pz_req, robot_center):
-        torque = -0.5 * self.m * self.g * (pz_req - robot_center[1])
+    def calculate_roll_torque(self, pz_req):
+        torque = -0.5 * self.m * self.g * pz_req
         return self.kp * torque
 
     def calculate_robot_center(self):
         l_foot_center = body_pos(self.model, self.data, "l_foot_pitch")
         r_foot_center = body_pos(self.model, self.data, "r_foot_pitch")
         return [l_foot_center[0], (r_foot_center[1] + l_foot_center[1]) / 2, l_foot_center[2]]
-
-    def calculate_hip_yaw_pos(self, sensor_data):
-        sensor_id = mj.mj_name2id(self.model, mj.mjtObj.mjOBJ_SENSOR, "accelerometer")
-        start = self.model.sensor_adr[sensor_id]
-        accel = np.array(sensor_data[start:start + 3])
-        accel /= np.linalg.norm(accel)
-        yaw = np.arctan2(accel[1], accel[2])
-        yaw = np.clip(yaw, -1, 1)
-        return yaw
 
     def make_floor_frame_from_foot(self, foot_prefix="left"):
         ids = [mj.mj_name2id(self.model, mj.mjtObj.mjOBJ_GEOM, f"{foot_prefix}_foot_{i}") for i in range(1, 5)]
@@ -83,7 +82,19 @@ class SimpleStabilizer:
         else:
             return left_frame
 
-    def step(self, desired_com_offset, sensor_data):
+    def get_pitch_torque(self):
+        return self.pitch_torque
+
+    def get_roll_torque(self):
+        return self.roll_torque
+
+    def get_hip_yaw_pos(self):
+        return self.hip_yaw_pos
+
+    def step(self, positions, acceleration, desired_com_offset):
+        self.data.qpos[:7] = [0] * 7
+        self.data.qpos[7:] = positions
+        mj.mj_step(self.model, self.data)
         robot_center = self.calculate_robot_center()
         frame = self.estimate_floor_frame_from_feet(robot_center)
         com = frame.T @ (self.com - robot_center)
@@ -93,6 +104,7 @@ class SimpleStabilizer:
         desired_accel_x = (-50 * (com[0] - desired_com[0])) / self.m
         desired_accel_y = (-50 * (com[1] - desired_com[1])) / self.m
         pz_req = [com[0] - desired_accel_x / omega2, com[1] - desired_accel_y / omega2]
-        self.pitch_torque = self.calculate_pitch_torque(pz_req[0], [0, 0, 0])
-        self.roll_torque = self.calculate_roll_torque(pz_req[1], [0, 0, 0])
-        self.hip_yaw_pos = self.calculate_hip_yaw_pos(sensor_data)
+        self.pitch_torque = self.calculate_pitch_torque(pz_req[0])
+        self.roll_torque = self.calculate_roll_torque(pz_req[1])
+        self.hip_yaw_pos = calculate_hip_yaw_pos(acceleration)
+        self.viewer.sync()
